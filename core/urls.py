5199a74856079c867fa869c9d88be8591daba105
mimimichaello <137781824+mimimichaello@users.noreply.github.com>
2024-05-06 15:34:44 +0300

    I set up authorization using JWT tokens

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from drf_yasg import openapi
 
 from django.conf import settings
@@ -28,6 +29,9 @@ urlpatterns = [
    path('admin/', admin.site.urls),
    path('api/v1/users/', include('user_profile.urls')),
    path('api/v1/followers/', include('followers.urls')),
+   path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+   path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+   path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from user_profile.views import UserListAPIView, ProfileAPIView
 
+
 urlpatterns = [
     path('users/', UserListAPIView.as_view(), name='user-list'),
     path('profile/<int:pk>', ProfileAPIView.as_view(), name='user-profile'),
 ]
+
